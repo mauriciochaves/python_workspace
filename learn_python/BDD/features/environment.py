cef2c1ead62add540259a246da0d29e5f16b49b2
Mauricio Chaves(pessoal) <mauricio.chaves.junior@live.com>
2018-12-06 17:14:09 -0300

    mudança no diretorio do driver

--- learn_python/BDD/features/environment.py
+++ learn_python/BDD/features/environment.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 
 def before_feature(context, feature):
-    context.driver = webdriver.Chrome('C:\\Projetos\\workspace\\python_workspace\\learn_python\\drivers\\chromedriver.exe')
+    context.driver = webdriver.Chrome('C:\\Projetos\\workspace\\python_workspace\\drivers\\chromedriver.exe')
     context.driver.implicitly_wait(10)
     context.driver.delete_all_cookies()
     context.driver.maximize_window()
