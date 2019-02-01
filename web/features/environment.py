44bdf4afff6ed915c9d3c235626988f18de265ac
Mauricio Chaves <35821839+mauriciochaves@users.noreply.github.com>
2019-02-01 08:22:54 -0200

    Update environment.py
    mudança no diretório para buscar o driver

--- web/features/environment.py
+++ web/features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def before_feature(context, feature):
-    context.driver = webdriver.Chrome('C:\\Projetos\\workspace\\drivers\\chromedriver.exe')
+    context.driver = webdriver.Chrome('..\\python_workspace\\drivers\\chromedriver.exe')
     context.driver.implicitly_wait(10)
     context.driver.delete_all_cookies()
     context.driver.maximize_window()
