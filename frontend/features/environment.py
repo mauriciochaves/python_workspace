43584faa2d45bc4f1c79f9d20f46b92dd6ef40c6
Mauricio Chaves <35821839+mauriciochaves@users.noreply.github.com>
2023-10-18 14:14:33 +0100

    Update environment.py
    update the call of webdrivers

--- frontend/features/environment.py
+++ frontend/features/environment.py
@@ -1,14 +1,17 @@
 import os
 import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 PARENT_PATH = os.path.abspath("..")
 if PARENT_PATH not in sys.path:
     sys.path.insert(0, PARENT_PATH)
-from util.utils import generate_report
+#from util.utils import generate_report
 
 def before_all(context):
-    context.base_url = context.config.userdata['URL']
-    context.driver = webdriver.Chrome('..\\drivers\\chromedriver.exe')
+context.base_url = "http://automationpractice.com/index.php"
+    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     context.driver.delete_all_cookies()
     context.driver.maximize_window()
 
